66add4a67f469fe786134111823f2fffe1ff2286
DE009 <1953622608@qq.com>
2024-03-16 12:56:08 +0800

    完成PC派生communication类，封装asr。1. 实现接受audio，asr后返回text。2. 实现封包函数，发送固定格式的控制信号dict

---
+++ pc_comm.py
@@ -0,0 +1,28 @@
+from communication import Communication
+from asr import Asr
+import json
+
+
+class PCCommunication(Communication):
+    def __init__(
+        self,
+    ) -> None:
+        super().__init__()
+        self.asr = Asr()
+        # 初始化控制信号格式
+        self.ctrl_sign_template = {"text": "", "end_chat": False, "action_ctrl": ""}
+
+    def send_ctrl_sign(self, text, end_chat=False, action=""):
+        # 发送控制型号
+        self.ctrl_sign_template["text"] = text
+        self.ctrl_sign_template["end_chat"] = end_chat
+        self.ctrl_sign_template["action_ctrl"] = action
+        self.send_dict(json.dumps(self.ctrl_sign_template))
+
+    def recv_audio_to_text(self):
+        # 接受audio，直接转换为text返回。
+        audio = self._recv_bytes()
+        print(audio)
+        print("进入ASR")
+        text = self.asr.audio_to_text(audio)
+        return text
